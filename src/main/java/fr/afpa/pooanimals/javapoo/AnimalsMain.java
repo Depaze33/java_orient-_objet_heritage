6e06e64e3d4ccd4ff7fdb0011944348f590551d0
Clarisse Otmani <clarisseotm@gmail.com>
2024-07-01 09:57:58 +0200

    Exercice terminé

--- src/main/java/fr/afpa/pooanimals/javapoo/AnimalsMain.java
+++ src/main/java/fr/afpa/pooanimals/javapoo/AnimalsMain.java
@@ -1,10 +1,11 @@
 package fr.afpa.pooanimals.javapoo;
 
-
+import java.util.ArrayList;
 
 public class AnimalsMain {
-
+public static ArrayList<Animal> animales = new ArrayList<>();
 	public static void main(String[] args) {
+		
 		Bird bird1 = new Bird("peroqquet", "jungle",20 );
 		Fish fish1 = new Fish("Requin", "eau", "froide");
 		Mammal mammal1 = new Mammal("Lion", "Jungle", "Brun");
@@ -12,14 +13,23 @@ public class AnimalsMain {
 		fish1.toString();
 		mammal1.toString();
 
-		System.out.println(bird1);
-		bird1.displayLocomotionType();
-		System.out.println(fish1);
-		fish1.displayLocomotionType();
-		System.out.println(mammal1);
-		mammal1.displayLocomotionType();
+		// System.out.println(bird1);
+		// bird1.displayLocomotionType();
+		// System.out.println(fish1);
+		// fish1.displayLocomotionType();
+		// System.out.println(mammal1);
+		// mammal1.displayLocomotionType();
 		
+		System.out.println("Liste des animaux: "); 
+// employee.forEach((n) -> print(n)); 
+for (Animal animal : animales) {
+	
+}
+		animales.add(bird1);
+ 		animales.add(fish1);
+ 		animales.add(mammal1);
 		
+ System.out.println("Les animaux sont " + (animales)); 
 
 
 	}
